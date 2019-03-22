f998c269f62cd83aa1e27930f03d38018029df8c
Simon Schulte <simonschulte@posteo.net>
2019-03-22 18:43:58 +0100

    add(endpoint): insert data into tile38

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 var app = require("express")();
 var http = require("http").Server(app);
 const bodyParser = require("body-parser");
+const redis = require('redis');
+const client = redis.createClient(9851, 'localhost');
 
 const PORT = 8080;
 
@@ -9,6 +11,37 @@ app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.put('/data/', function(req, res) {
+  const data = req.body.data;
+  // Vll timestmap zu data hinzufügen
+  const timestamp = Date.now()
+  const collectionName = req.body.collectionName;
+  const geofenceName = req.body.geofenceName;
+  // tile38 verbindung
+  try {
+    client.send_command(
+      "SET",
+      [collectionName, geofenceName, "OBJECT", JSON.stringify(data)],
+      (err, reply) => {
+        if (err) {
+          console.log("error geojson", err ,reply);
+        } else {
+          console.log("inserting geojson", reply);
+          // antworten mit IDs
+          res.sendStatus(200, reply)
+        }
+      }
+    );
+  } catch (e) {
+    res.sendStatus(400, 'There was an error updating the information: ' + e)
+  }
+  // Marks code reinschreiben
+
+  
+
+
 });
 
 http.listen(PORT, function() {
