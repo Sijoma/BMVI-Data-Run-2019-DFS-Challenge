a9b1435735f30437f58155ff823ba3b744c78ba7
Nico Steffens <n.steffens@uni-muenster.de>
2019-03-22 23:27:06 +0100

    Intermediate Changes

--- src/js/bos_main.js
+++ src/js/bos_main.js
@@ -1,8 +1,39 @@
-let map;
-
 $(document).ready(function () {
-    map = initMap()
+    let map = L.map('small_map').setView([52.529, 13.377], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: config.mapboxKey
+    }).addTo(map);
+
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+
+    drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        },
+        draw: {
+            marker: false,
+            circle: false,
+            rectangle: false,
+            polyline: false
+        }
+    });
+    map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (e) {
+        var type = e.layerType,
+            layer = e.layer;
     
-    console.log("ready!");
+        // Do whatever else you need to. (save to db; add to map etc) -> ok!
+        map.addLayer(layer);
+    
+        map.removeControl(drawControl);
+        
+    });
 
 });
+
