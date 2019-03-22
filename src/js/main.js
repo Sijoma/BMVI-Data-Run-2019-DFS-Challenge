4ee076a9caf7714219d9886f6d97dfc58e9c9af6
Nico Steffens <n.steffens@uni-muenster.de>
2019-03-22 19:33:41 +0100

    Basic Map added with sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
+/**
+ * 
+ *  Main frontend entrypoint
+ *  include stuff here 
+ */
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+// Basic map init. ToDo: Setup connection to backend
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: config.mapboxKey
-}).addTo(mymap);
+let map;
+
+$(document).ready(function () {
+    map = initMap()
+    
+    console.log("ready!");
+
+
+});
---
+++ src/js/map.js
@@ -0,0 +1,17 @@
+var initMap = function(){
+
+    let map = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: config.mapboxKey
+    }).addTo(map);
+
+    // TODO: Add layer für Flugverbotszonen
+    
+    return map;
+
+
+}
