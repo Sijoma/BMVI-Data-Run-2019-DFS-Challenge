424bef55a937358cd58f0afe0b5c519358ed5bc7
Nico Steffens <n.steffens@uni-muenster.de>
2019-03-22 20:15:31 +0100

    Added leaflet.draw + datepicker

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,6 @@
-var initMap = function(){
+var initMap = function () {
 
-    let map = L.map('mapid').setView([51.505, -0.09], 13);
+    let map = L.map('mapid').setView([52.529, 13.377], 13);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,8 +9,37 @@ var initMap = function(){
         accessToken: config.mapboxKey
     }).addTo(map);
 
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+   
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        },
+        draw: {
+            polygon: false,
+            marker: false,
+            circle: false,
+            rectangle: false
+        }
+    });
+    map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (e) {
+        var type = e.layerType,
+            layer = e.layer;
+        if (type === 'marker') {
+            // Do marker specific actions
+        }
+        // Do whatever else you need to. (save to db; add to map etc) -> ok!
+        map.addLayer(layer);
+     });
+
+
+
+
     // TODO: Add layer für Flugverbotszonen
-    
+
     return map;
 
 
